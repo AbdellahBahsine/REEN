4e2723d2de97aa8b2c46922e6a5aafa736c2dffb
Paylo01 <paylo_@outlook.com>
2021-03-25 15:46:08 +0100

    created mega menu

--- main.js
+++ main.js
@@ -5,3 +5,25 @@ hamburgerMenu.addEventListener("click", function() {
   mobileMenu.classList.toggle("visible");
 })
 
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+  });
+});
